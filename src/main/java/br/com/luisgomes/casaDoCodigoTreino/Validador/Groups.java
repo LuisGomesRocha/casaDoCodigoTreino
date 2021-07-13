aadf284a0300124e00a76afe390b1a19ddb4ecff
Luis Augusto Gomes <79930400+LuisGomesRocha@users.noreply.github.com>
2021-07-13 20:10:07 -0300

    Criação do validador genérico
    <p align="justify"> :robot: A classe VerificaCampoDuplicadoValidator irá implementar uma query que irá buscar o valor no bando de dados para que seja comparado ao email de entada retornado a presença ou não do email pesquisado no banco de dados. Uma vez a notação criada a mesma será implementada na classe AutorRequest @VerificaCampoDuplicado(attribute = "email", clazz = Autor.class). A mesma lógica pode ser utilizada no cadastro de categoria.:robot: </p>
    
    
    <h2 align="center">
        Exemplo de aplicação: AutorRequest e CategoriaRequest:
    </h2>
    
    ```	
    	Ex.:   
    	@VerificaCampoDuplicado(attribute = "nome",clazz = Categoria.class)
    	private String nome;
    	
    	      OU
    	@NotBlank @Email
    	@VerificaCampoDuplicado(attribute = "email", clazz = Autor.class)
    	private String email;   
    	
    ```
    
    
    
    <h2 align="center">
        VerificaCampoDuplicadoValidator
    </h2>
    
    ```
    public class VerificaCampoDuplicadoValidator
            implements ConstraintValidator<VerificaCampoDuplicado,Object> {
    
        private String campo;
        private Class<?> clazz;
    
        @PersistenceContext
        private EntityManager manager;
    
        @Override
        public void initialize(VerificaCampoDuplicado parameters) {
    
            this.campo = parameters.attribute();
            this.clazz = parameters.clazz();
        }
    
        @Override
        public boolean isValid(Object objetoValidacao, ConstraintValidatorContext context) {
    
            Query query = manager.createQuery("SELECT 1 FROM "+ clazz.getName() + " where " + campo + " =:valor");
            query.setParameter("valor", objetoValidacao);
    
            var resultList = query.getResultList();
    
            return resultList.size() > 0 ? false: true;
    
        }
    }
    ```

---
+++ src/main/java/br/com/luisgomes/casaDoCodigoTreino/Validador/Groups.java
@@ -0,0 +1,14 @@
+package br.com.luisgomes.casaDoCodigoTreino.Validador;
+
+public interface Groups {
+
+	public interface Categoria {
+	}
+
+	public interface Autor {
+	}
+
+	public interface Pais {
+	}
+}
+
