6d070e93266d6f9e815f8016c7164eff0335b1e9
Luis Augusto Gomes <79930400+LuisGomesRocha@users.noreply.github.com>
2021-07-13 16:13:59 -0300

    Add files via upload

---
+++ src/main/java/br/com/luisgomes/casaDoCodigoTreino/Autor/AutorRepository.java
@@ -0,0 +1,8 @@
+package br.com.luisgomes.casaDoCodigoTreino.Autor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+}
+
---
+++ src/main/java/br/com/luisgomes/casaDoCodigoTreino/Autor/AutorRequest.java
@@ -0,0 +1,41 @@
+package br.com.luisgomes.casaDoCodigoTreino.Autor;
+
+import java.util.Locale;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+public class AutorRequest {
+
+	@NotBlank
+	private String nome;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	@Size(max = 400)
+	private String descricao;
+
+	public AutorRequest(@NotBlank String nome, @NotBlank @Email String email,
+			@NotBlank @Size(max = 400) String descricao) {
+		this.nome = nome;
+		this.setEmail(email);
+		this.descricao = descricao;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email.toLowerCase(Locale.ROOT);
+	}
+
+	
+
+	public Autor toAutor() {
+		return new Autor(this.nome, this.email, this.descricao);
+	}
+}
