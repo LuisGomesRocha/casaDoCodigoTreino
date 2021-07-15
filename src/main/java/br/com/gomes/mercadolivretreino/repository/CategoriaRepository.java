8731262d69f0bbf2c888017193be005d21481fe5
Luis Augusto Gomes <79930400+LuisGomesRocha@users.noreply.github.com>
2021-07-15 17:50:53 -0300

    Delete src/main/java/br/com/gomes/mercadolivretreino directory

--- src/main/java/br/com/gomes/mercadolivretreino/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.gomes.mercadolivretreino.repository;
-
-import br.com.gomes.mercadolivretreino.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
